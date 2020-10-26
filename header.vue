fafc40c32aa08e682b934a8c5164017e04b8cf3d
Jay B <thtan0407@gmail.com>
2020-10-26 20:16:19 +0700

    Check OG:Image

---
+++ header.vue
@@ -0,0 +1,78 @@
+<template>
+    <header>
+        <div class="header d-flex align-items-center">
+            <div class="menu-icon menu-icon-right d-block d-lg-none">
+                <a href="#menu-sidebar"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+            </div>
+            <a href="#" class="logo">
+                <img src="images/logo.png" alt="">
+            </a>
+            <nav class="menu">
+                <ul>
+                    <li>
+                        <a href="#">Giáo trình</a>
+                    </li>
+                    <li>
+                        <a href="#">Luyện tập</a>
+                    </li>
+                    <li>
+                        <a href="#">Thư viện</a>
+                    </li>
+                    <li>
+                        <a href="#">Đăng tin tuyển sinh</a>
+                    </li>
+                    <li>
+                        <a href="#">Thi thử JLPT</a>
+                    </li>
+                    <li>
+                        <a href="#">Cộng đồng</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="login view-info d-none d-md-block">
+
+                <a class="d-none" href="#">Đăng nhập</a>
+                <a class="d-none" href="#">Đăng ký</a>
+
+                <a class="info-user" href="javascript:void(0)">
+                    <i class="fa fa-info"></i>
+                    Thông tin tài khoản
+                </a>
+                <ul>
+                    <li>
+                        <span>
+                            <i class="fa fa-user"></i>
+                                Họ tên:
+                        </span>
+                        Nguyễn Văn A
+                    </li>
+                    <li>
+                        <span>
+                            <i class="fa fa-book"></i>
+                            Điểm số:
+                        </span>
+                        36/40
+                    </li>
+                    <li>
+                        <span>
+                            <i class="fa fa-line-chart"></i>
+                            Lượt truy cập:
+                        </span>
+                        8 lần
+                    </li>
+                    <li>
+                        <a href="javascript:void(0)">
+                            <span>
+                                <i class="fa fa-sign-out"></i>
+                                Logout
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="menu-icon d-block d-xl-none ml-auto ml-md-2">
+                <a href="#menu-mobile"><i class="fa fa-bars" aria-hidden="true"></i></a>
+            </div>
+        </div>
+    </header>
+</template>
