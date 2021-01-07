029b5cb27512ae72f4a6f77bca94b89d9bf1952b
Jay B <thtan0407@gmail.com>
2021-01-07 12:09:10 +0700

    Add video youtube

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-
 $(window).bind('load', function () {
-    $('.toggle-sub').click(function () {
-        $(this).parents('.menu-item').stop().toggleClass('active');
-        $(this).siblings('.sub-menu').stop().slideToggle();
-    });
-    $('#menu-mobile').mmenu({
-        extensions: ["position-right", "position-front"]
-    });
-    $('#menu-sidebar').mmenu({
-        extensions: ["position-left", "position-front"]
-    });
+	$('.toggle-sub').click(function () {
+		$(this).parents('.menu-item').stop().toggleClass('active');
+		$(this).siblings('.sub-menu').stop().slideToggle();
+	});
+	$('#menu-mobile').mmenu({
+		extensions: ["position-right", "position-front"]
+	});
+	$('#menu-sidebar').mmenu({
+		extensions: ["position-left", "position-front"]
+	});
 });
